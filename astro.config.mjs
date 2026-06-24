96561feeab839c13ae69980599676c38d2912a0e
VanessaRagains <Vanessa.Ragains@gmail.com>
2026-06-24 10:32:34 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://beyondbarscoaching.org',
+  site: 'https://vanessaragains.github.io',
+  base: '/beyondbarscoaching'
   integrations: [sitemap()],
 });
