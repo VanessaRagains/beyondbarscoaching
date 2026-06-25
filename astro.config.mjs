e91470ceca4c87984a4e3c9f8adbfb4e0d27bebd
Jessie Keck <jessie.keck@gmail.com>
2026-06-24 22:29:29 -0700

    Re-add cname and update config for upcoming site switch

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://vanessaragains.github.io',
-  base: '/beyondbarscoaching/',
+  site: 'https://beyondbarscoaching.org',
+  base: '/',
   integrations: [sitemap()],
 });
